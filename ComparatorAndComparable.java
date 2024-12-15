1c6c60d6020e0d23db8f8adc70f96afcce4024e2
Raj <charan.raj@capgemini.com>
2024-12-15 21:26:25 +0530

    changes

--- ComparatorAndComparable.java
+++ ComparatorAndComparable.java
@@ -1,7 +1,9 @@
 package javacode;
 import java.util.*;
 
-class Student implements Comparable<Student>{
+class Student 
+// implements Comparable<Student>
+{
     private String name;
     private int age;
     public Student(String name, int age) {
@@ -20,9 +22,9 @@ class Student implements Comparable<Student>{
     public void setAge(int age) {
         this.age = age;
     }
-    public int compareTo(Student that) {
-        return this.getAge() > that.getAge() ? 1 : -1;
-    }
+    // public int compareTo(Student that) {
+    //     return this.getAge() > that.getAge() ? 1 : -1;
+    // }
     public String getString(){
         return this.getName()+":"+this.getAge();
     }
@@ -31,13 +33,22 @@ class Student implements Comparable<Student>{
 
 public class ComparatorAndComparable {
     public static void main(String[] args) {
+        Comparator<Student> com = new Comparator<Student>() {
+
+            public int compare(Student o1, Student o2) {
+                return o1.getAge() > o2.getAge() ? 1 : -1;
+            }
+            
+        };
+
         List<Student> Students = new ArrayList<>();
         Students.add(new Student("Basava", 20));
         Students.add(new Student("Chandru", 19));
         Students.add(new Student("Shiva", 24));
         Students.add(new Student("Kariya", 18));
 
-        Collections.sort(Students);
+        // Collections.sort(Students);
+        Collections.sort(Students, com);
         for(Student s:Students){
             System.out.println(s.getString());
         }
