b087fccaaedc12d81cbd5d6a4b093a210d6f2829
Charan Raj <charan.raj@sap.com>
2025-01-22 16:43:09 +0530

    change

---
+++ Spring JDBC/SpringJDBCpostgresql/src/main/java/com/telusko/SpringJDBCEx/repo/StudentRepo.java
@@ -0,0 +1,53 @@
+package com.telusko.SpringJDBCEx.repo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.telusko.SpringJDBCEx.model.Student;
+
+@Repository
+public class StudentRepo {
+	private JdbcTemplate jdbc;
+
+	public JdbcTemplate getJdbc() {
+		return jdbc;
+	}
+	@Autowired
+	public void setJdbc(JdbcTemplate jdbc) {
+		this.jdbc = jdbc;
+	}
+
+
+	public void save(Student s) {
+		String sql = "insert into student (rollNo, name, marks) values (?,?,?)";
+		int rows = jdbc.update(sql, s.getRollNo(), s.getName(), s.getMarks());
+		System.out.println(rows+" added");
+	}
+
+	public List<Student> findAll() {
+		String sql = "select * from student";
+//		RowMapper<Student> rowmap = ( rs,  rowNum) -> {
+//				Student s = new Student();
+//				s.setRollNo(rs.getInt("rollNo"));
+//				s.setName(rs.getString("name"));
+//				s.setMarks(rs.getInt("marks"));
+//				return s;
+//		};
+		return jdbc.query(sql, ( rs,  rowNum) -> {
+			Student s = new Student();
+			s.setRollNo(rs.getInt("rollNo"));
+			s.setName(rs.getString("name"));
+			s.setMarks(rs.getInt("marks"));
+			return s;
+		});
+	}
+
+	
+}
---
+++ Spring JDBC/SpringJDBCpostgresql/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student (rollNo, name, marks) values (101, 'charan', 99);
+insert into student (rollNo, name, marks) values (102, 'ramesh', 23);
+insert into student (rollNo, name, marks) values (103, 'mahesh', 54);
